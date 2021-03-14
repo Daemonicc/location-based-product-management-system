64a2f74c7c0726a34e741b8bbfba898e1b87ce69
Igunnu Emmanuel <40716797+Daemonicc@users.noreply.github.com>
2021-03-14 19:55:03 +0100

    Update geocode.js

--- helpers/geocode.js
+++ helpers/geocode.js
@@ -3,7 +3,7 @@ const NodeGeocoder = require('node-geocoder');
 const options = {
 	provider: process.env.GEOCODER_PROVIDER,
 	httpAdapter: 'https',
-	apiKey: 'AIzaSyC_UJ0AmcWrgghiYYjZEbTELLrK0F6szSc',
+	apiKey: 'process.env.GEOCODER_API',
 	formatter: null,
 };
 
